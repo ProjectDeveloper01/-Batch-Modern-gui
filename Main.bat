91366d5d178a2ac33943c42bbb4adbcfad276f7f
ProjectDeveloper01 <127253208+ProjectDeveloper01@users.noreply.github.com>
2023-07-21 12:19:22 -0500

    Update Main.bat
    Some add-ons that  improves modern-looking of the GUI.

--- Main.bat
+++ Main.bat
@@ -1,12 +1,15 @@
 @echo off
 :MENU
 cls
-echo =====================
-echo    My Batch GUI
-echo =====================
-echo 1. Open Google
+echo =======================
+echo    Modern batch GUI
+echo =======================
+echo 1. Open Chrome
+rem 1) start "chrome.exe"
 echo 2. Show Credits
+rem 2) show program credits
 echo 3. Exit
+rem 3) exit or close the modern gui or application(program)
 echo =====================
 set /p choice=Enter your choice (1-3): 
 
@@ -30,7 +33,7 @@ goto :MENU
 
 :ShowCredits
 echo Credits:
-echo - Created by [Your Name]
+echo - Created by [https://github.com/ProjectDeveloper01/]
 echo - Version 1.0
 echo - Date: %date%
 echo - Time: %time%
